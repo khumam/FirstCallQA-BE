2547561e8dbc27af8556c12983f138a7fb306aea
Khoerul Umam <khoerul.umam@gotoko.co.id>
2023-08-09 08:43:37 +0700

    move to sqlite

--- src/requests/user.request.ts
+++ src/requests/user.request.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-const PostValidation = (req: Request, res: Response, next: NextFunction) => {
+const PostValidation = async (req: Request, res: Response, next: NextFunction) => {
   let errors = []
   if (req.body.username.length < 6 || req.body.username.length > 48) {
     errors.push('Username length should be not less than 6 chars and not more than 48 chars.');
@@ -10,6 +12,9 @@ const PostValidation = (req: Request, res: Response, next: NextFunction) => {
   }
   if (req.body.last_name.length > 55) {
     errors.push('Last name length should be not more than 55 chars.');
+  }
+  if (await prisma.user.findFirst({ where: { username: req.body.username } }) != null) {
+    errors.push('Username exists');
   }
 
   if (errors.length > 0) {
@@ -46,7 +51,20 @@ const UpdateValidation = (req: Request, res: Response, next: NextFunction) => {
   next();
 };
 
+const UserValidation = async (req: Request, res: Response, next: NextFunction) => {
+  const user = await prisma.user.findFirst({ where: { id: req.params.id } })
+  if (user == null) {
+    const response = {
+      status: 'Failed',
+      message: 'User does not exists'
+    }
+    return res.status(400).json(response);
+  }
+  next();
+}
+
 export {
   PostValidation,
-  UpdateValidation
+  UpdateValidation,
+  UserValidation
 };
--- src/routes/api.route.ts
+++ src/routes/api.route.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import HealthController from "../controllers/health.controller";
 import UserController from "../controllers/user.controller";
-import { PostValidation, UpdateValidation } from "../requests/user.request";
+import { PostValidation, UpdateValidation, UserValidation } from "../requests/user.request";
 
 const apiRoute = Router();
 apiRoute.get('/health/checkup', HealthController.checkup);
 
 apiRoute.get('/user', UserController.getAll);
-apiRoute.get('/user/:id', UserController.getById);
-apiRoute.post('/user/store', PostValidation ,UserController.store);
-apiRoute.put('/user/update/:id', UpdateValidation, UserController.update);
-apiRoute.delete('/user/delete/:id', UserController.delete);
+apiRoute.get('/user/:id', UserValidation, UserController.getById);
+apiRoute.post('/user/store', PostValidation, UserController.store);
+apiRoute.put('/user/update/:id', UpdateValidation, UserValidation, UserController.update);
+apiRoute.delete('/user/delete/:id', UserValidation, UserController.delete);
 
 export default apiRoute;
